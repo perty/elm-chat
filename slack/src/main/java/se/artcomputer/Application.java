d5607760266c913cfbe2100b3dd665722a1a0f7e
Per Lundholm <per@artcomputer.se>
2020-09-20 17:58:24 +0200

    Added a Micronaut server

---
+++ slack/src/main/java/se/artcomputer/Application.java
@@ -0,0 +1,10 @@
+package se.artcomputer;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}
