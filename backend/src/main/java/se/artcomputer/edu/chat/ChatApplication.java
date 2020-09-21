d6b0bb4593ecc204b36f720802dab84036929b43
Per Lundholm <per@artcomputer.se>
2020-09-21 22:04:11 +0200

    Reorganized into gradle project with frontend and backend.

---
+++ backend/src/main/java/se/artcomputer/edu/chat/ChatApplication.java
@@ -0,0 +1,13 @@
+package se.artcomputer.edu.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatApplication.class, args);
+	}
+
+}
