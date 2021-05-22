69b8391f6a82ef47d1f8595a18f1dc2078e2de5b
Per Lundholm <per@artcomputer.se>
2021-05-22 07:21:37 +0200

    Moved all server communication to Api

--- frontend/src/main/elm/Main.elm
+++ frontend/src/main/elm/Main.elm
@@ -1,11 +1,9 @@
 module Main exposing (main)
 
+import Api exposing (loadChannelMessages, loadChannels)
 import Browser
-import Json.Decode
-import Json.Decode.Pipeline exposing (required)
 import Model exposing (Message, Model, initialModel)
 import Msg exposing (Msg(..))
-import RemoteData.Http
 import Time
 import View exposing (view)
 
@@ -22,47 +20,6 @@ main =
 init : () -> ( Model, Cmd Msg )
 init _ =
     ( initialModel, loadChannels )
-
-
-loadChannels : Cmd Msg
-loadChannels =
-    RemoteData.Http.get "http://localhost:8080/api/v1/channels" HandleChannelResponse channelDecoder
-
-
-channelDecoder : Json.Decode.Decoder (List String)
-channelDecoder =
-    Json.Decode.list Json.Decode.string
-
-
-loadChannelMessages : String -> Cmd Msg
-loadChannelMessages channel =
-    let
-        url =
-            "http://localhost:8080/api/v1/channels/" ++ channel ++ "/messages"
-    in
-    RemoteData.Http.get url HandleChannelMessagesResponse channelMessagesDecoder
-
-
-channelMessagesDecoder : Json.Decode.Decoder (List Message)
-channelMessagesDecoder =
-    Json.Decode.list channelMessageDecoder
-
-
-channelMessageDecoder : Json.Decode.Decoder Message
-channelMessageDecoder =
-    Json.Decode.succeed Message
-        |> required "author" Json.Decode.string
-        |> required "content" Json.Decode.string
-        |> required "created" decodeTime
-
-
-decodeTime : Json.Decode.Decoder Time.Posix
-decodeTime =
-    Json.Decode.int
-        |> Json.Decode.andThen
-            (\ms ->
-                Json.Decode.succeed <| Time.millisToPosix ms
-            )
 
 
 
